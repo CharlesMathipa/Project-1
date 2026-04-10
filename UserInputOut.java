5f2cc718431dce93faf83af1021cf68029291879
CharlesMathipa <ST10511987@rcconnect.edu.za>
2026-04-10 04:25:56 +0200

    asking the user questions or what to input with scanner methods

--- UserInputOut.java
+++ UserInputOut.java
@@ -5,5 +5,18 @@ public class UserInputOut {
         Scanner scanner = new Scanner(System.in);
         
         Login login = new Login();
+
+        System.out.println("--- REGISTRATION ---");
+        System.out.print("Enter your first name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Enter your last name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Enter a username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Enter a password: ");
+        String password = scanner.nextLine();
     }
 }        
