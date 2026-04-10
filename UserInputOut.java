87ee5429ef0d2b6b58a8df33daa4e8e6e2249ff7
CharlesMathipa <ST10511987@rcconnect.edu.za>
2026-04-10 04:28:44 +0200

    Implement user registration and login functionality in UserInputOut.java

--- UserInputOut.java
+++ UserInputOut.java
@@ -18,5 +18,27 @@ public class UserInputOut {
 
         System.out.print("Enter a password: ");
         String password = scanner.nextLine();
+
+        String registrationMessage = login.registerUser(username, password, firstName, lastName);
+        System.out.println(registrationMessage);
+
+        if (registrationMessage.equals("The two above conditions have been met and the user has been registered successfully.")) {
+            
+            System.out.println("\n--- LOGIN ---");
+            System.out.print("Enter your username to login: ");
+            String loginUsername = scanner.nextLine();
+
+            System.out.print("Enter your password to login: ");
+            String loginPassword = scanner.nextLine();
+
+            // Check if login is valid
+            boolean isValidLogin = login.loginUser(loginUsername, loginPassword);
+            
+            // Get the final status message and print it
+            String loginStatusMessage = login.returnLoginStatus(isValidLogin);
+            System.out.println(loginStatusMessage);
+        }
+
+        scanner.close();
     }
 }        
