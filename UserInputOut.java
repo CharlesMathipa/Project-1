76f97e7e7caa764256f82c8e1208a6f99f38e237
CharlesMathipa <ST10511987@rcconnect.edu.za>
2026-04-10 04:22:56 +0200

    created an instance of a new login class

---
+++ UserInputOut.java
@@ -0,0 +1,7 @@
+public class UserInputOut {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        Login login = new Login();
+    }
+}        
